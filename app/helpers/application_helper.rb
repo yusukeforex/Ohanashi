9f736245532d32a699f9a1f6ee59c1fdef14259a
yusukeforex <momijina@icloud.com>
2020-01-13 13:47:42 +0900

    finish broadcasting room msg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,3 @@
 module ApplicationHelper
-  def gravatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    url = "https://gravatar.com/avatar/#{gravatar_id}.id}.png"
-  end
+
 end
